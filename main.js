4096fbff16ac8c8920170c1c5c1b0324bdd90e3f
mispelaur <spencer.lauren.m@gmail.com>
2017-06-18 23:28:52 +0100

    GET /poll test

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const count = {}
 const userVoteCount = {}
 const app = require('./createServer')(count, userVoteCount)
-const port = process.env.PORT || 8081
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`Listening on port ${port}`)
---
+++ test/server_test.js
@@ -0,0 +1,27 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const expect = require('chai').expect
+const should = chai.should()
+const sortCountDesc = require('../sortCountDesc')
+
+chai.use(chaiHttp)
+
+describe('GET /poll', () => {
+  it('it should output the number of votes per contestant in descending order', (done) => {
+    const initialCount = {
+      'a': 8000000,
+      'b': 2000000,
+      'c': 6000000,
+      'd': 4000000
+    }
+    const app = require('../createServer')(initialCount, {})
+    const sorted = sortCountDesc(initialCount)
+    chai.request(app)
+      .get('/poll')
+      .end((err, res) => {
+        res.should.have.status(200)
+        expect(res.body).to.deep.equal(sorted)
+        done()
+      })
+  })
+})
